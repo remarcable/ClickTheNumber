dac04e90d138f184d3df9cd335e2f1836a1847ba
Marc Nitzsche <marc.nitzsche@live.de>
2016-11-12 23:43:57 +0100

    Change name in footer and make font in numberboxes bolder

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer className="github">
-      Made with ❤︎ by <a rel="noopener noreferrer" target="_blank" href="https://github.com/lightningboss/ClickTheNumber">LightningBoss</a>
+      Made with ❤︎ by <a rel="noopener noreferrer" target="_blank" href="https://github.com/lightningboss/ClickTheNumber">Marc</a>
     </footer>
   );
 }
