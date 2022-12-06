919f2d23e83195975b04e0bc372e5f3ad3ca51d1
Marc N <git@marcnitzsche.de>
2022-12-06 11:41:25 +0000

    Update react

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "normalize.css";
 
-import App from './components/App';
-import './index.css';
+import App from "./components/App";
+import "./index.css";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
